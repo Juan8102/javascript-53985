f435233095ac68ef45f475bf2f743c6b2d2c35db
Juan8102 <juandedioscaminacc@gmail.com>
2024-03-27 10:23:17 -0300

    Mas ejercicios hechos

---
+++ ejercicios/funciones.js
@@ -0,0 +1,30 @@
+//EJERCICIO 1
+/*let numero = parseInt(prompt("Ingresa un número para obtener su cuadrado."))
+
+function sacarCuadrado (numero) {
+    return numero ** 2;
+}
+
+let resultado = sacarCuadrado(numero);
+
+alert(resultado)*/
+
+//EJERCICIO 2
+/*let numero1 = parseInt(prompt("Ingresa un número."));
+let numero2 = parseInt(prompt("Ingresa otro número."));
+
+function verificacion (numero1, numero2) {
+    if (numero1 > numero2) {
+        return 1;
+    }
+    else if (numero2 > numero1) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+let resultado = verificacion(numero1, numero2)
+
+alert(resultado)*/
