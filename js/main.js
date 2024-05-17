b88d30a50c8e4eaf314548129dfdf48b2ccb6454
Juan8102 <juandedioscaminacc@gmail.com>
2024-05-17 13:18:18 -0300

    Ajustes

--- js/main.js
+++ js/main.js
@@ -1,54 +1,39 @@
-let isAPIWorking;
+//Función que obtiene los productos del archivo products.json
+function getJSONProducts() {
+    fetch("../products.json")
+    .then((response) => {
+        return response.json();
+    })
+    .then((responseJSON) => {
+        products = responseJSON;
+        currentArray = products;
 
-let currentArray;
-const products = JSON.parse(localStorage.products);
-function getProducts() {
-    return new Promise((resolve, reject) => {
         const APIError = Math.random();
-
         setTimeout(() => {
             if (APIError > 0.05) {
-                resolve({products: products})
+                resolveAPI();
             }
             else {
-                reject()
+                errorLoadingScreen();
             }
         }, 2000);
     })
 }
-const APIResponse = getProducts();
-
-APIResponse
-    .then((response) => {
-        currentArray = response.products;
-        isAPIWorking = true;
-        resultsIndicator.style.display = "block";
-        
-        renderProducts(currentArray);
-        verifyPage();
-    })
-    .catch(() => {
-        isAPIWorking = false;
-        errorLoadingScreen();
-    });
+getJSONProducts();
 
 //Simulación de un error en la API
+let isAPIWorking = false;
 const loadingContainer = document.getElementById("loading__container");
 const loadingElements = document.getElementById("loading__elements");
 const loadingIcon = document.getElementById("loading__icon");
 
-function errorLoadingScreen() {
-    loadingIcon.setAttribute("color", "yellow");
-    setTimeout(() => {
-        loadingIcon.setAttribute("color", "red");
-        setTimeout(() => {
-            loadingElements.innerHTML = `
-                <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="red"><path d="M479.98-280q14.02 0 23.52-9.48t9.5-23.5q0-14.02-9.48-23.52t-23.5-9.5q-14.02 0-23.52 9.48t-9.5 23.5q0 14.02 9.48 23.52t23.5 9.5ZM453-433h60v-253h-60v253Zm27.27 353q-82.74 0-155.5-31.5Q252-143 197.5-197.5t-86-127.34Q80-397.68 80-480.5t31.5-155.66Q143-709 197.5-763t127.34-85.5Q397.68-880 480.5-880t155.66 31.5Q709-817 763-763t85.5 127Q880-563 880-480.27q0 82.74-31.5 155.5Q817-252 763-197.68q-54 54.31-127 86Q563-80 480.27-80Zm.23-60Q622-140 721-239.5t99-241Q820-622 721.19-721T480-820q-141 0-240.5 98.81T140-480q0 141 99.5 240.5t241 99.5Zm-.5-340Z"/></svg>
-                <h3>API load time exceeded</h3>
-                <h4>Try reloading the page</h4>
-            `;
-        }, 1000);
-    }, 1000);
+function resolveAPI() {
+    isAPIWorking = true;
+    resultsIndicator.style.display = "block";
+    
+    getCart();
+    renderProducts(currentArray);
+    verifyPage();
 }
 
 function loadingScreen() {
@@ -68,7 +53,33 @@ function loadingScreen() {
     }, 1000);
 }
 
+function errorLoadingScreen() {
+    loadingIcon.setAttribute("color", "yellow");
+    setTimeout(() => {
+        const APIError = Math.random();
+        if (APIError > 0.5) {
+            resolveAPI();
+        }
+
+        loadingIcon.setAttribute("color", "red");
+        setTimeout(() => {
+            const APIError = Math.random();
+            if (APIError > 0.75) {
+                resolveAPI();
+            }
+
+            loadingElements.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="red"><path d="M479.98-280q14.02 0 23.52-9.48t9.5-23.5q0-14.02-9.48-23.52t-23.5-9.5q-14.02 0-23.52 9.48t-9.5 23.5q0 14.02 9.48 23.52t23.5 9.5ZM453-433h60v-253h-60v253Zm27.27 353q-82.74 0-155.5-31.5Q252-143 197.5-197.5t-86-127.34Q80-397.68 80-480.5t31.5-155.66Q143-709 197.5-763t127.34-85.5Q397.68-880 480.5-880t155.66 31.5Q709-817 763-763t85.5 127Q880-563 880-480.27q0 82.74-31.5 155.5Q817-252 763-197.68q-54 54.31-127 86Q563-80 480.27-80Zm.23-60Q622-140 721-239.5t99-241Q820-622 721.19-721T480-820q-141 0-240.5 98.81T140-480q0 141 99.5 240.5t241 99.5Zm-.5-340Z"/></svg>
+                <h3>API load time exceeded</h3>
+                <h4>Try reloading the page</h4>
+            `;
+        }, 1000);
+    }, 1000);
+}
+
 //Renderización de productos
+let products;
+let currentArray;
 const productsContainer = document.getElementById("cards__container");
 
 let renderStart = 0;
@@ -80,7 +91,7 @@ function renderProducts(array) {
     array = array.slice(renderStart, renderEnd);
 
     for (const product of array) {
-        if (product.stock > 0) {
+        if (product.available) {
             //Contenedor de toda la información del producto
             const div = document.createElement("div");
             div.className = "card";
@@ -89,25 +100,11 @@ function renderProducts(array) {
             const imgDiv = document.createElement("div");
             imgDiv.id = "product__image__container";
 
-            let image;
-            if (product.image !== "") { //Si el producto no tiene una imágen asignada, se sustituirá por un texto
-                const img = document.createElement("img");
-                img.setAttribute("src", `${product.image}`);
-                img.id = "card__img";
-                image = img;
-            }
-            else {
-                const div = document.createElement("div");
-                const p = document.createElement("p");
-
-                p.innerText = "Image not found"
-                p.id = "img__replace__text";
-
-                div.id = "img__replace";
-                div.append(p);
-                image = div;
-            }
-            imgDiv.append(image);
+            const img = document.createElement("img");
+            img.setAttribute("src", `${product.image}`);
+            img.setAttribute("title", `${product.title}'s logo`);
+            img.id = "card__img";
+            imgDiv.append(img);
 
             //Textos
             const textsDiv = document.createElement("div");
@@ -118,27 +115,26 @@ function renderProducts(array) {
             const h4 = document.createElement("h4");
             h4.innerText = `$${product.price}`;
 
-            const p = document.createElement("p");
-            p.innerText = `Stock: ${product.stock}`;
-
-            textsDiv.append(h3, h4, p);
+            textsDiv.append(h3, h4);
 
             //Botón
+            const button = document.createElement("button");
+            button.className = "card__button";
+
+            const imgButton = document.createElement("img");
+            imgButton.setAttribute("src", "media/add-to-cart.png");
+
+            button.append(imgButton);
+            button.addEventListener("click", () => {
+                addToCart(product);
+            })
+
             const isProductInCart = cart.find((el) => el.title === product.title);
-            if (isProductInCart === undefined) {
-                const button = document.createElement("button");
-                button.className = "card__button";
-
-                const imgButton = document.createElement("img");
-                imgButton.setAttribute("src", "media/add-to-cart.png");
-
-                button.append(imgButton);
-                button.addEventListener("click", () => {
-                    addToCart(product);
-                })
-
-                textsDiv.append(button);
+            if (isProductInCart !== undefined) {
+                button.className = "cart__card__button";
             }
+            
+            textsDiv.append(button);
 
             div.append(imgDiv, textsDiv);
             productsContainer.append(div);
@@ -219,36 +215,34 @@ function verifyPage(action) {
 const resultsIndicator = document.getElementById("filters__indicator");
 const priceRangeText = document.getElementById("price__range__text");
 const priceRangeSlider = document.getElementById("price__range");
-setTimeout(() => {
-    if (isAPIWorking) {
-        priceRangeSlider.addEventListener("change", filterByPrice);
-        priceRangeSlider.addEventListener("input", () => {
-            const priceRange = priceRangeSlider.value * 6;
-            if (priceRange !== 0) {
-                priceRangeText.innerText = `Less than $${priceRange}`;
-            }
-            else {
-                priceRangeText.innerText = "Any price";
-            }
-        });
+priceRangeSlider.addEventListener("change", filterByPrice);
+priceRangeSlider.addEventListener("input", () => {
+    const priceRange = priceRangeSlider.value * 6;
+    if (priceRange !== 0) {
+        priceRangeText.innerText = `Less than $${priceRange}`;
     }
-}, 2000);
+    else {
+        priceRangeText.innerText = "Any price";
+    }
+});
 
 function filterByPrice() {
-    const priceRange = priceRangeSlider.value * 6;
-    if (priceRange !== 0) { //Si el filtro de price es aplicado, se realizará el filtrado correspondiente
-        currentArray = products.filter((el) => el.price <= priceRange);
-        setTimeout(() => {
-            resultsIndicator.innerText = `${currentArray.length} results`;
-        }, 1000);
-        verifyPage(3);
-    }
-    else { //Si el filtro de price no es aplicado, se mostrarán todos los products
-        setTimeout(() => {
-            resultsIndicator.innerText = "All games";
-        }, 1000);
-        currentArray = products;
-        verifyPage(3);
+    if (isAPIWorking) {
+        const priceRange = priceRangeSlider.value * 6;
+        if (priceRange !== 0) { //Si el filtro de price es aplicado, se realizará el filtrado correspondiente
+            currentArray = products.filter((el) => el.price <= priceRange);
+            setTimeout(() => {
+                resultsIndicator.innerText = `${currentArray.length} results`;
+            }, 1000);
+            verifyPage(3);
+        }
+        else { //Si el filtro de price no es aplicado, se mostrarán todos los products
+            setTimeout(() => {
+                resultsIndicator.innerText = "All games";
+            }, 1000);
+            currentArray = products;
+            verifyPage(3);
+        }
     }
 }
 
@@ -265,7 +259,6 @@ function getCart() {
         cartIndicator.innerText = `${cart.length}`;
     }
 }
-getCart();
 
 //Contenedores
 const cartContainer = document.getElementById("cart__sidebar__container");
@@ -287,6 +280,8 @@ closeCartButton.addEventListener("click", () => showCart(2));
 overlay.addEventListener("click", () => showCart(3));
 
 function showCart(action) {
+    let cartVerified = false;
+
     let cartContainerClass;
     let bodyScrollClass;
     let overlayClass;
@@ -294,17 +289,32 @@ function showCart(action) {
         cartContainerClass = "shown";
         bodyScrollClass = "noscroll";
         overlayClass = "overlay";
+
+        if (isAPIWorking) {
+            renderCart();
+        }
+        else {
+            cartVerified = true;
+            const verifyCart = setInterval(() => {
+                if (isAPIWorking) {
+                    renderCart();
+                    clearInterval(verifyCart);
+                }
+            }, 500);
+        }
     }
     else { //Si se presionó el botón de cerrar el carrito o se hizo click fuera del carrito
         cartContainerClass = "closed";   
         bodyScrollClass = "scroll";
         overlayClass = "no-overlay";
+        
+        if (cartVerified) {
+            clearInterval(verifyCart);
+        }
     }
     cartContainer.className = cartContainerClass;
     bodyScroll.className = bodyScrollClass;
     overlay.className = overlayClass;
-
-    renderCart();
 }
 
 function renderCart() {
@@ -471,59 +481,62 @@ function emptyCart() {
 //Busqueda de productos
 const searchProductsContainer = document.getElementById("search__bar__games");
 const searchBar = document.getElementById("search__bar");
-setTimeout(() => {
+searchBar.addEventListener("input", searchProducts);
+searchBar.addEventListener("focus", searchProducts);
+searchBar.addEventListener("blur", () => {
     if (isAPIWorking) {
-        searchBar.addEventListener("input", searchProducts);
-        searchBar.addEventListener("focus", searchProducts);
-        searchBar.addEventListener("blur", () => {
-            if (searchBar.value.length === 0) {
-                setTimeout(() => {
-                    searchProductsContainer.innerHTML = "";
-                }, 250);
-            }
-        });
+        if (searchBar.value.length === 0) {
+            setTimeout(() => {
+                searchProductsContainer.innerHTML = "";
+            }, 250);
+        }
     }
-}, 2000);
+});
 
 function searchProducts() {
-    if (searchBar.value.length > 0) {
-        setTimeout(() => {
-            searchProductsContainer.innerHTML = "";
-            const foundProducts = products.filter((el) => el.title.toLowerCase().includes(searchBar.value.toLowerCase()));
-            for (const product of foundProducts) {
-                //Contenedor de toda la información de los productos buscados
-                const div = document.createElement("div");
-                div.className = "search__bar__games__item";
+    if (isAPIWorking) {
+        if (searchBar.value.length > 0) {
+            setTimeout(() => {
+                searchProductsContainer.innerHTML = "";
+                const foundProducts = products.filter((el) => el.title.toLowerCase().includes(searchBar.value.toLowerCase()));
+                for (const product of foundProducts) {
+                    if (product.available) {
+                        //Contenedor de toda la información de los productos buscados
+                        const div = document.createElement("div");
+                        div.className = "search__bar__games__item";
+        
+                        //Contenedor de los textos
+                        const textsDiv = document.createElement("div");
+        
+                        const h4 = document.createElement("h4");
+                        h4.innerText = `${product.title}`;
+        
+                        h4.className = "search__bar__games__item__texts";
+        
+                        const h5 = document.createElement("h5");
+                        h5.innerText = `$${product.price}`;
+                        h5.className = "search__bar__games__item__texts";
+        
+                        textsDiv.append(h4, h5);
+                        div.append(textsDiv);  
 
-                //Contenedor de los textos
-                const textsDiv = document.createElement("div");
+                        //Botón
+                        const isProductInCart = cart.find((el) => el.title === product.title)
+                        if (isProductInCart === undefined) {
+                            const imgButton = document.createElement("img");
+                            imgButton.id = "search__add__to__cart__button";
+                            imgButton.setAttribute("src", "media/white-add-to-cart.png");
+                            imgButton.addEventListener("click", () => {
+                                addToCart(product);
+                            })
+            
+                            div.append(imgButton);
+                        }
 
-                const h4 = document.createElement("h4");
-                h4.innerText = `${product.title}`;
-
-                h4.className = "search__bar__games__item__texts";
-
-                const h5 = document.createElement("h5");
-                h5.innerText = `$${product.price}`;
-                h5.className = "search__bar__games__item__texts";
-
-                const h52 = document.createElement("h5");
-                h52.innerText = `Stock: ${product.stock}`;
-                h52.className = "search__bar__games__item__texts";
-
-                textsDiv.append(h4, h5, h52);
-
-                //Botón
-                const imgButton = document.createElement("img");
-                imgButton.id = "search__add__to__cart__button";
-                imgButton.setAttribute("src", "media/white-add-to-cart.png");
-                imgButton.addEventListener("click", () => {
-                    addToCart(product);
-                })
-
-                div.append(textsDiv, imgButton);           
-                searchProductsContainer.append(div);
-            }
-        }, 250);
+                        searchProductsContainer.append(div);
+                    }
+                }
+            }, 250);
+        }
     }
 }
